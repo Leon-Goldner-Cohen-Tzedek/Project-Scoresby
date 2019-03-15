05606ba1ca5af52308650a36a1efb683483182f2
Leon Goldner Cohen Tzedek <leonkatz21@gmail.com>
2019-03-14 20:16:33 -0700

    TODO: sprite sheet and basic backgrounds

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main()
   sf::ContextSettings settings;
   settings.antialiasingLevel = 255;
 
-  sf::RenderWindow window(sf::VideoMode(300, 400), "Scoresby", sf::Style::Default, settings);
+  sf::RenderWindow window(sf::VideoMode(1000, 1000), "Scoresby", sf::Style::Default, settings);
 
   sf::CircleShape shape(100.f);
   shape.setFillColor(sf::Color(255, 255, 255));
@@ -26,6 +26,25 @@ int main()
       window.clear();
       if (event.type == sf::Event::Closed)
         window.close();
+      if (event.type == sf::Event::KeyPressed)
+      {
+        if(event.key.code == sf::Keyboard::W)
+        {
+          carSprite.move(sf::Vector2f(0.f, -5.f));
+        }
+        if(event.key.code == sf::Keyboard::S)
+        {
+          carSprite.move(sf::Vector2f(0.f, 5.f));
+        }
+        if(event.key.code == sf::Keyboard::A)
+        {
+          carSprite.move(sf::Vector2f(-5.f, 0.f));
+        }
+        if(event.key.code == sf::Keyboard::D)
+        {
+          carSprite.move(sf::Vector2f(5.f, 0.f));
+        }
+      }
 
       else window.draw(shape); window.draw(carSprite);
       window.display();
